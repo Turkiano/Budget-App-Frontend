65a02c3b4da1d5584e0d252ce442ad6a4b956e38
Turkiano <turki.saeed.ksa@gmail.com>
2024-12-15 11:27:04 +0300

    Applying UseState to the tasks

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,22 @@ import './App.css';
 import { TodoItem } from './TodoItem';
 
 function App() {
-  const tasks = ['task01', 'task02', 'task03'];
-  const [counter, setCounter] = useState(0);
+  const [tasks, setTasks] = useState(['task01', 'task02', 'task03']);
 
-  const handleIncrement = () => {
-    setCounter(counter + 1);
+  const handleAddTask = () => {
+    const task = 'hit the gym';
+    const newTask = [...tasks, task];
+    setTasks(newTask);
   };
   //Return DOM using Jsx elements
   return (
     <div className="App">
-      <h3>{counter}</h3> 
       <ul>
         {tasks.map((task) => {
-          return <TodoItem title={task} />;
+          return <TodoItem title={task} key={task} />;
         })}
-        <button onClick={handleIncrement}>Add</button> 
       </ul>
+        <button onClick={handleAddTask}>Add</button>
     </div>
   );
 }
