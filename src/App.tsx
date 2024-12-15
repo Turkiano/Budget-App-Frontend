12d8b58c7be04390c9d57709bc98d1800fdceee5
Turkiano <turki.saeed.ksa@gmail.com>
2024-12-15 10:08:31 +0300

    useState for Rendering Elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import { TodoItem } from './TodoItem';
 
 function App() {
-  const tasks = ["task01", "task02", "task03"]
+  const tasks = ['task01', 'task02', 'task03'];
+  const [counter, setCounter] = useState(0);
 
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
   //Return DOM using Jsx elements
   return (
-    
-      <div className='App'>
-        <ul>
-        {tasks.map((task) =>{
-          return <TodoItem title= {task} />
+    <div className="App">
+      <h3>{counter}</h3> 
+      <ul>
+        {tasks.map((task) => {
+          return <TodoItem title={task} />;
         })}
-
-        </ul>
-      </div>
-    
+        <button onClick={handleIncrement}>Add</button> 
+      </ul>
+    </div>
   );
 }
 
--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 // Define the props type
 type TodoItemProps = {
   title: string;
@@ -5,7 +7,13 @@ type TodoItemProps = {
 
 // Apply object structuring from the props
 export function TodoItem({ title }: TodoItemProps) {
-  console.log('title: ', title); //to test our props
+  //01. Declare the variable that has the useRef method
+  const hasLogged = React.useRef(false);
+//02. add the condition to log only first render
+  if (!hasLogged.current) {
+    console.log('title: ', title); //to test our props
+    hasLogged.current = true;
+  }
   return (
     <>
       <li>
