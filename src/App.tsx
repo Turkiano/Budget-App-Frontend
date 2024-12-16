a8405e4a77d9489311388b5e2ef55a40f840ab7d
Turkiano <turki.saeed.ksa@gmail.com>
2024-12-16 13:28:25 +0300

    Form [type submit] - App is ready

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
   const [tasks, setTasks] = useState(['task01', 'task02', 'task03']);
   const [userInput, setUserInput] = useState("");
 
-  const handleAddTask = () => {
+  const handleAddTask = (e) => {
+    e.preventDefault()
     const newTask = [...tasks, userInput];
     setTasks(newTask);
   };
@@ -19,13 +20,15 @@ function App() {
   //Return DOM using Jsx elements
   return (
     <div className="App">
+      <form onSubmit={handleAddTask}>
       <input type="text" placeholder="Add a new task" onChange={handelChange} />
-      <button onClick={handleAddTask}>Add</button>
+      <button type = "submit">Add</button>
       <ul>
         {tasks.map((task) => {
           return <TodoItem title={task} key={task} />;
         })}
       </ul>
+        </form>
     </div>
   );
 }
