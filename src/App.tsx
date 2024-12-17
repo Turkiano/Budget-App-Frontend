7852bd4d7dc9d9dded1e4a6eb8cb8aa55272e602
Turkiano <turki.saeed.ksa@gmail.com>
2024-12-17 10:08:38 +0300

    Remove button is working

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import { TodoItem } from './TodoItem';
 
 function App() {
   const [tasks, setTasks] = useState(['task01', 'task02', 'task03']);
-  const [userInput, setUserInput] = useState("");
+  const [userInput, setUserInput] = useState('');
 
-  const handleAddTask = (e) => {
-    e.preventDefault()
+  const AddTask = (e) => {
+    e.preventDefault();
+
     const newTask = [...tasks, userInput];
     setTasks(newTask);
   };
@@ -17,18 +18,31 @@ function App() {
     setUserInput(UserValue);
   };
 
+  const handleRemove = (item) => {
+    setTasks(tasks.filter((task) => task !== item));
+  };
+
   //Return DOM using Jsx elements
   return (
     <div className="App">
-      <form onSubmit={handleAddTask}>
-      <input type="text" placeholder="Add a new task" onChange={handelChange} />
-      <button type = "submit">Add</button>
-      <ul>
-        {tasks.map((task) => {
-          return <TodoItem title={task} key={task} />;
-        })}
-      </ul>
-        </form>
+      <form onSubmit={AddTask}>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          onChange={handelChange}
+        />
+        <button type="submit">Add</button>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+               <TodoItem title={task} key={task} />
+              <button type="button" onClick={() => handleRemove(task)}>
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
