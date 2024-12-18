c92725f00a62064e66eb7ef06e62e5d5bb9981de
Turkiano <turki.saeed.ksa@gmail.com>
2024-12-18 11:18:31 +0300

    learning how to do edit & save functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,10 @@
-import { useState } from 'react';
-import './App.css';
-import { TodoItem } from './TodoItem';
+import React, { useState } from 'react';
 
-function App() {
-  const [tasks, setTasks] = useState(['task01', 'task02', 'task03']);
-  const [userInput, setUserInput] = useState('');
+const TodoApp = () => {
+  cosnt[(todos, setTods)] = useState([
+    { id: 1, text: 'learn React', isEditing: false },
+    { id: 2, text: 'Build a Todo App', isEditing: false },
+  ]);
 
-  const AddTask = (e) => {
-    e.preventDefault();
-
-    const newTask = [...tasks, userInput];
-    setTasks(newTask);
-  };
-
-  const handelChange = (e) => {
-    const UserValue = e.target.value;
-    setUserInput(UserValue);
-  };
-
-  const handleRemove = (item) => {
-    setTasks(tasks.filter((task) => task !== item));
-  };
-
-  const handleUpdate = (item) => {
-    setTasks(tasks.filter((task) => task !== item));
-  };
-  //Return DOM using Jsx elements
-  return (
-    <div className="App">
-      <form onSubmit={AddTask}>
-        <input
-          type="text"
-          placeholder="Add a new task"
-          onChange={handelChange}
-        />
-        <button type="submit">Add</button>
-        <ul>
-          {tasks.map((task, index) => (
-            <li key={index}>
-               <TodoItem title={task} key={task} />
-               <button type='button' onClick={handleUpdate}>Update</button>
-              <button type="button" onClick={() => handleRemove(task)}>
-                Remove
-              </button>
-
-            </li>
-          ))}
-        </ul>
-      </form>
-    </div>
-  );
-}
-
-export default App;
+  const [editText, setEditText] = useState('');
+};
