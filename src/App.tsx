1a3f786ebd4009f675091d656d9e62d7ac491be6
Turkiano <turki.saeed.ksa@gmail.com>
2024-12-20 17:10:29 +0300

    AddTask Function - finito

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,31 @@ import { TodoItem } from './TodoItem';
 
 function App() {
   const [tasks, setTasks] = useState([
-    { id: 1, title: "Learn React", isEditing: false },
-    { id: 2, title: "Build a Todo App", isEditing: false },
+    { id: 1, title: 'Learn React', isEditing: false },
+    { id: 2, title: 'Build a Todo App', isEditing: false },
   ]);
   const [userInput, setUserInput] = useState('');
 
   const AddTask = (e) => {
     e.preventDefault();
-  //validation for preventing empty tasks
-    if (userInput.trim() === "") {
-      alert("Task title cannot be empty");
+    //validation for preventing empty tasks
+    if (userInput.trim() === '') {
+      alert('Task title cannot be empty');
       return;
     }
-    
-  //each task should be an object with properties like id, title, and isEditing
+
+    //each task should be an object with properties like id, title, and isEditing
     const newTask = {
       id: tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1, // Generate a new unique id
       title: userInput.trim(), // Use the trimmed user input as the title
       isEditing: false, // Default `isEditing` to false
     };
-  
-    setTasks([...tasks, newTask]); // Add the new task to the main tasks array
-    setUserInput(""); // Clear the input field after adding the new task
-  };
-  
 
-  const handelChange = (e) => {
+    setTasks([...tasks, newTask]); // Add the new task to the main tasks array
+    setUserInput(''); // Clear the input field after adding the new task
+  };
+
+  const handleChange = (e) => {
     const UserValue = e.target.value;
     setUserInput(UserValue);
   };
@@ -48,19 +47,20 @@ function App() {
         <input
           type="text"
           placeholder="Add a new task"
-          onChange={handelChange}
-          value={userInput}
+          onChange={handleChange}
+          value={userInput} //to ensure the input field is controlled and syncs with the userInput state.
         />
         <button type="submit">Add</button>
         <ul>
           {tasks.map((task) => (
             <li key={task.id}>
-               <TodoItem title={task.title} />
-               <button type='button' onClick={handleUpdate}>Update</button>
+              <TodoItem title={task.title} />
+              <button type="button" onClick={handleUpdate}>
+                Update
+              </button>
               <button type="button" onClick={() => handleRemove(task)}>
                 Remove
               </button>
-
             </li>
           ))}
         </ul>
