bc929f0708e52a8af07406f808dcf840dc777a85
Turkiano <turki.saeed.ksa@gmail.com>
2024-12-21 09:25:20 +0300

    first component - Button

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+import { Button } from './Components/Button';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+     <h1>Budget App</h1>
+     <Button/>
+    </div>
+  );
+}
+
+export default App;
