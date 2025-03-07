3f437ab8482b86cb95e97d05ca693af9e1bd47e1
Turkiano <turki.saeed.ksa@gmail.com>
2025-03-07 19:38:41 +0300

    sign up form is working

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,94 +1,94 @@
-import { Link } from 'react-router-dom';
-import api from '../api/api';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { Link } from "react-router-dom";
+import api from "../api/api";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export function SignUp() {
-  const [user, setUser] = useState({
-    firstName: '',
-    lastName: '',
-    phone: '',
-    email: '',
-    password: '',
-  });
-  const [error, setError] = useState<string | null>(null);
-  console.log('user inputs:', user);
+    const [user, setUser] = useState({
+        firstName: "",
+        lastName: "",
+        phone: "",
+        email: "",
+        password: ""
+    });
 
-  const handleSignUp = async () => {
-    try {
-      const res = await api.post('/users/signup', user);
-      return res.data;
-    } catch (error) {
-      console.error('Sign-up error:', error);
-      setError('Something went wrong! Please try again.');
-      return Promise.reject(new Error('Something went wrong!!'));
-    }
-  };
+    const [error, setError] = useState<string | null>(null);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+    console.log("User inputs:", user);
 
-    setUser((prevUser) => ({
-      ...prevUser,
-      [name]: value,
-    }));
-  };
+    const handleSignUp = async () => {
+        try {
+            const res = await api.post("/users/signup", user);
+            return res.data;
+        } catch (error) {
+            console.error("Sign-up error:", error);
+            setError("Something went wrong! Please try again.");
+            return Promise.reject(new Error("Something went wrong!!"));
+        }
+    };
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    setError(null); // Reset error state
-    await handleSignUp();
-  };
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setUser((prevUser) => ({
+            ...prevUser,
+            [name]: value
+        }));
+    };
 
-  return (
-    <div className="SignUpContainer">
-      <h1>SignUp Page</h1>
-      <div className="SignUpForm">
-        <form onSubmit={handleSubmit}>
-          <div>
-            <input
-              name="firstName"
-              type="text"
-              placeholder="firstName"
-              value={user.firstName}
-              onChange={handleChange}
-            />
-            <input
-              name="lastName"
-              type="text"
-              placeholder="lastName"
-              value={user.lastName}
-              onChange={handleChange}
-            />
-            <input
-              name="phone"
-              type="text"
-              placeholder="phone"
-              value={user.phone}
-              onChange={handleChange}
-            />
-            <input
-              name="email"
-              type="email"
-              placeholder="email"
-              value={user.email}
-              onChange={handleChange}
-            />
-            <input
-              name="password"
-              type="password"
-              placeholder="password"
-              value={user.password}
-              onChange={handleChange}
-            />
-          </div>
-          {error && <p className="error-message">{error}</p>}
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        setError(null); // Reset error state
+        await handleSignUp();
+    };
 
-          <button type="submit">SignUp</button>
-          <p>
-            Have an Account <Link to="/login"> Login</Link>
-          </p>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className="SignUpContainer">
+            <h1>Sign Up Page</h1>
+            <div className="SignUpForm">
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <input
+                            name="firstName"
+                            type="text"
+                            placeholder="First Name"
+                            value={user.firstName}
+                            onChange={handleChange}
+                        />
+                        <input
+                            name="lastName"
+                            type="text"
+                            placeholder="Last Name"
+                            value={user.lastName}
+                            onChange={handleChange}
+                        />
+                        <input
+                            name="phone"
+                            type="text"
+                            placeholder="Phone"
+                            value={user.phone}
+                            onChange={handleChange}
+                        />
+                        <input
+                            name="email"
+                            type="email"
+                            placeholder="Email"
+                            value={user.email}
+                            onChange={handleChange}
+                        />
+                        <input
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            value={user.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    {error && <p className="error-message">{error}</p>}
+                    <button type="submit">Sign Up</button>
+                    <p>
+                        Have an account? <Link to="/login">Login</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
 }
